9a66dbb3b26367f679975ca83b1502f72be1ade6
MFirtina61 <114576186+MFirtina61@users.noreply.github.com>
2023-01-10 10:34:00 +0100

    begin van contact pagina

--- Eind opdracht Museum/script.js
+++ Eind opdracht Museum/script.js
@@ -1,72 +1,10 @@
-p1y=p2y=40;
-pt=10;
-ph=100;
-bx=by=50;
-bd=6;
-xv=yv=4;
-score1 =score2=0;
-ais=2;
-window.onload=function(){
-    c=document.getElementById('gc');
-    cc=c.getContext('2d');
-    setInterval(update,1000/30);
-    c.addEventListener('mousemove',function(e){
-        p1y = e.offsetY;
-        console.log(p1y);
-    });
+var hartjeContainer = document.querySelector('.comments section');
+var hartje = document.querySelector('.comments section i');
+console.log(1);
 
+function liken() {
+    hartjeContainer.classList.toggle('liked')
+    console.log(2)
 }
-function reset(){
-    bx=c.width/2;
-    by=c.height/2;
-    xv=xv;
-    yv=3;
-}
-function update(){
-    bx+=xv;
-    by+=yv;
-    if(by<0 && yv<0) {
-        yv=-yv;
-    }
-    if(by>c.height && yv>0) {
-        yv=-yv;
-    }
-    if(bx<0) {
-        if(by>p1y && by<p1y+ph){
-            xv=-xv;
-            dy=by-(p1y+ph/2);
-            yv = dy*0.3; 
-             
-        } else{
-            score2++;
-            reset();
-        }
-    }
-    if(bx>c.width) {
-        if(by>p2y && by<p2y+ph){
-            xv=-xv;
-            dy=by-(p2y+ph/2);
-            yv = dy*0.3; 
-             
-        } else{
-            score1++;
-            reset();
-        }
-    }
-
-    if(p2y+ph/2<by){
-        p2y+=ais;
-    } else {
-        p2y-=ais
-    }
-
-    cc.fillStyle='#525252';
-    cc.fillRect(0,0,c.width,c.height);
-    cc.fillStyle='#fff';
-    cc.fillRect(0,p1y,pt,ph);
-    cc.fillRect(c.width-pt,p2y,pt,ph);
-    cc.fillRect(bx-bd/2,by-bd/2,bd,bd)
-    cc.fillText(score1,100,100);
-    cc.fillText(score2,c.width-100,100);
-
-}
+hartje.addEventListener("click", liken);
+console.log(3);
