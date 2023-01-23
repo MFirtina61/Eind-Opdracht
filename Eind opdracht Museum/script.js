6b0bf07ded5c28566784b84706d86bd0a2717986
MFirtina61 <114576186+MFirtina61@users.noreply.github.com>
2023-01-23 20:38:44 +0100

    Add files via upload
    Bijna klaar

--- Eind opdracht Museum/script.js
+++ Eind opdracht Museum/script.js
@@ -1,10 +1,58 @@
-var hartjeContainer = document.querySelector('.comments section');
-var hartje = document.querySelector('.comments section i');
-console.log(1);
-
-function liken() {
-    hartjeContainer.classList.toggle('liked')
-    console.log(2)
+class App {
+  constructor() {
+    this.hartjeContainer = document.querySelector(".comments section");
+    this.hartje = document.querySelector(".comments section i");
+  }
+  liken() {
+    this.hartjeContainer.classList.toggle("liked");
+    console.log(2);
+  }
+  unliken() {
+    this.hartje.addEventListener("click", liken());
+    console.log(3);
+  }
 }
-hartje.addEventListener("click", liken);
-console.log(3);
+const app = new App();
+
+
+// class App {
+//   constructor() {
+//     this.hartjeContainer = document.querySelectorAll(".comments section");
+//     this.hartje = document.querySelectorAll(".comments section i");
+//   }
+//   liken() {
+//     tis.hartjeContainer[i].classList.toggle("liked");
+//     console.log(2);
+//   }
+//   unliken() {
+//     this.hartje[i].addEventListener("click", liken());
+//     console.log(3);
+//   }
+// }
+// const app = new App();
+
+
+// var hartjeContainer = document.querySelectorAll(".comments section");
+// var hartje = document.querySelectorAll(".comments section i");
+
+
+
+
+// for (i = 0; i < hartjeContainer.length; ++i) {
+//   console.log("1");
+// }
+
+// for (i = 0; i < hartje.length; ++i) {
+//   // hartje[i].classList.toggle("liked");
+//   console.log("2");
+// }
+
+// function liken() {
+
+// hartje[i].classList.toggle("liked");
+// tis.addEventListener("click", liken);
+
+// }
+
+
+// // console.log(hartjeContainer.length);
